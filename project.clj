4bff69c190a8d365f39157c8a878a982d9ca792e
Danny Wilson <danny@prime.vc>
2015-01-23 18:30:28 +0100

    Update ring-cors

--- project.clj
+++ project.clj
@@ -9,7 +9,7 @@
                  [prime/utils "0.1.0-SNAPSHOT"
                   :exclusions [org.slf4j/slf4j-api org.slf4j/slf4j-log4j12]]
                  [prime/filerepository-cassandra "0.1.0-SNAPSHOT"]
-                 [ring-cors "0.1.3"]
+                 [ring-cors "0.1.6"]
                  [com.taoensso/timbre "3.2.1"]
                  [commons-codec "1.8"]]
   :profiles {:test {:dependencies [[containium/containium "0.1.0-SNAPSHOT"
